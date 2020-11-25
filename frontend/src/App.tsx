cc44c2907bef5c0121506d9f1d31130efd0dcb90
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-11-25 04:09:36 +0330

    Setup React. Install redux and react-redux.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FC, Fragment } from "react";
+
+const App: FC = () => {
+  return (
+    <Fragment>
+      <h1>Welcome To ProShop</h1>
+    </Fragment>
+  );
+};
+
+export default App;
