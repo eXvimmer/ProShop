5d8b2a83a13d8872d5feaec85a8d2850416b7bd1
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-11-25 17:11:51 +0330

    Install axios. Add the package.json file to root and make an express server at backend folder which serves the products @ /api/products and @ /api/products/:id.

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const products = require("../data/products");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find(p => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(port, () =>
+  console.log(`✅ Server is running on port ${port}`)
+);
