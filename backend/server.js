cdf7869747aa5f62ef1d58a984363324b869638c
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-11-27 08:53:19 +0330

    Create custom error handling for server.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ import dotenv from "dotenv";
 // NOTE: files should have .js extension
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import {
+  errorHandler,
+  notFound,
+} from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -11,11 +15,21 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use("/api/products", productRoutes);
-
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+
+app.use("/api/products", productRoutes);
+
+/* ANCHOR
+ * fallback for 404s
+ */
+app.use(notFound);
+
+/* ANCHOR
+ * Custom error handling middleware
+ */
+app.use(errorHandler);
 
 app.listen(PORT, () =>
   console.log(
