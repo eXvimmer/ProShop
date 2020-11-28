838a3c6baa7de770d80a219ffd58c8affc1f6b48
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-11-28 13:01:55 +0330

    Create a middleware (pre save) in User schema to hash passwords before registering users. Add route and controller to register a user.

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -27,6 +27,41 @@ export const authUser = asyncHandler(async (req, res) => {
 });
 
 /* ANCHOR
+ * @DESC        Register a new user
+ * @ROUTE       POST /api/users
+ * @ACCESS      Public
+ */
+export const registerUser = asyncHandler(async (req, res) => {
+  const { email, password, name } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+/* ANCHOR
  * @DESC        Get user profile
  * @ROUTE       GET /api/users/profile
  * @ACCESS      Private
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   authUser,
   getUserProfile,
+  registerUser,
 } from "../controllers/userControllers.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.route("/").post(registerUser);
 router.route("/login").post(authUser);
 router.route("/profile").get(protect, getUserProfile);
 
