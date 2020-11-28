bdcf28a93efeeb90693ce4c8c285566f659d2689
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-11-28 12:12:46 +0330

    Create generateToken util function. Add it to authUser controller to generate and send back a token to the user.

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 /* ANCHOR
  * @DESC        Auth user and get token
@@ -17,8 +18,7 @@ export const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      /* TODO: add token */
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
