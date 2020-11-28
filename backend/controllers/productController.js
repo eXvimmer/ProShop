86469b8216d4d3ad74238d00b75c291c530a96c1
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-11-28 10:03:08 +0330

    Move all controllers from product routes to the productControllers file.

---
+++ backend/controllers/productController.js
@@ -0,0 +1,29 @@
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
+
+/* ANCHOR
+ * @DESC Fetch all products
+ * @ROUTE Get /api/products
+ * @ACCESS Public
+ */
+export const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  // res.status(401);
+  // throw new Error("NOT Authorized");
+  res.json(products);
+});
+
+/* ANCHOR
+ * @DESC Fetch single product
+ * @ROUTE Get /api/products/:id
+ * @ACCESS Public
+ */
+export const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    return res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,38 +1,13 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
-import Product from "../models/productModel.js";
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-/* ANCHOR
- * @DESC Fetch all products
- * @ROUTE Get /api/products
- * @ACCESS Public
- */
-router.route("/").get(
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    // res.status(401);
-    // throw new Error("NOT Authorized");
-    res.json(products);
-  })
-);
+router.route("/").get(getProducts);
 
-/* ANCHOR
- * @DESC Fetch single product
- * @ROUTE Get /api/products/:id
- * @ACCESS Public
- */
-router.route("/:id").get(
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      return res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
+router.route("/:id").get(getProductById);
 
 export default router;
