b7238cb4b7c786673bc4dbfffd2066fe3b63f7ec
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-12-01 10:33:04 +0330

    Create controller and router for getting an order by it's id @ /api/orders/:id (private).

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -38,3 +38,22 @@ export const addOrderItems = asyncHandler(async (req, res) => {
     });
   }
 });
+
+/* ANCHOR
+ * @DESC    Get Order by ID
+ * @ROUTE   GET /api/orders/:id
+ * @ACCESS  Private
+ */
+export const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error(`Order not found`);
+  }
+});
