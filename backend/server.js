aac0aef5473476c95979993ac7e58ce681ecdaa1
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-12-05 11:26:55 +0330

    Install multer and create config and route for uploading (JPEG, JPG or PNG) image to uploads directory.

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 // NOTE: files should have .js extension
@@ -5,6 +6,7 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 import {
   errorHandler,
   notFound,
@@ -31,17 +33,28 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/uploads", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-/* ANCHOR
+/* 
+ NOTE
+ * __dirname is a commonJS thing. It's not available
+ * if you're using ES modules. So you have to mimick this ⬇
+*/
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+/* 
+ ANCHOR
  * fallback for 404s
  */
 app.use(notFound);
 
-/* ANCHOR
+/* 
+ ANCHOR
  * Custom error handling middleware
  */
 app.use(errorHandler);
